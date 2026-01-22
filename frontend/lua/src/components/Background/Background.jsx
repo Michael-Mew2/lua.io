ad987cea0b49418010b1c0c2666f1de1bc9d4fbb
Michael-Mew2 <michael.mdnev@gmail.com>
2026-01-22 22:18:32 +0100

    [hotfix]: fixed NaN-Error at the background-component

--- frontend/lua/src/components/Background/Background.jsx
+++ frontend/lua/src/components/Background/Background.jsx
@@ -5,21 +5,21 @@ export default function Background() {
   return (
     <div className={styles.background}>
       <b></b>
-      <b className={styles.span-2}></b>
-      <b className={styles.span-3}></b>
-      <b className={styles.span-4}></b>
-      <b className={styles.span - 5}></b>
-      <b className={styles.span - 4}></b>
-      <b className={styles.span - 3}></b>
-      <b className={styles.span - 2}></b>
+      <b className={styles["span-2"]}></b>
+      <b className={styles["span-3"]}></b>
+      <b className={styles["span-4"]}></b>
+      <b className={styles["span-5"]}></b>
+      <b className={styles["span-4"]}></b>
+      <b className={styles["span-3"]}></b>
+      <b className={styles["span-2"]}></b>
       <b></b>
-      <b className={styles.span - 2}></b>
-      <b className={styles.span - 3}></b>
-      <b className={styles.span - 4}></b>
-      <b className={styles.span - 5}></b>
-      <b className={styles.span - 4}></b>
-      <b className={styles.span - 3}></b>
-      <b className={styles.span - 2}></b>
+      <b className={styles["span-2"]}></b>
+      <b className={styles["span-3"]}></b>
+      <b className={styles["span-4"]}></b>
+      <b className={styles["span-5"]}></b>
+      <b className={styles["span-4"]}></b>
+      <b className={styles["span-3"]}></b>
+      <b className={styles["span-2"]}></b>
       <b></b>
     </div>
   );
