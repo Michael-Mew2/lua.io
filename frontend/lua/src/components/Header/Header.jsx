906d90e7bdec8c2237e21fb85a968e202292094b
Michael-Mew2 <michael.mdnev@gmail.com>
2026-02-02 17:44:07 +0100

    [feature]: add new logo to the site

--- frontend/lua/src/components/Header/Header.jsx
+++ frontend/lua/src/components/Header/Header.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
-import { Burger, Container, Group } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
+import { useState } from "react";
+import { Anchor, Burger, Container, Group, Image } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 // import { MantineLogo } from '@mantinex/mantine-logo';
-import classes from './Header.module.css';
+import classes from "./Header.module.css";
 
 const links = [
-  { link: '/about', label: 'Features' },
-  { link: '/pricing', label: 'Pricing' },
-  { link: '/learn', label: 'Learn' },
-  { link: '/community', label: 'Community' },
+  { link: "/about", label: "Features" },
+  { link: "/pricing", label: "Pricing" },
+  { link: "/learn", label: "Learn" },
+  { link: "/community", label: "Community" },
 ];
 
 export default function HeaderSimple() {
@@ -33,7 +33,18 @@ export default function HeaderSimple() {
   return (
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
-       {/*  <MantineLogo size={28} /> */}
+        <Anchor href="/" className={classes.logo}>
+          <Group gap={1}>
+            <Image
+              src="./icons/Tononónimo-icon-solo.svg"
+              h={40} // Festgelegte Höhe in Pixeln (verhindert Springen beim Laden)
+              w="auto" // Breite passt sich automatisch dem Seitenverhältnis an
+              fit="contain" // Sorgt dafür, dass das SVG sauber skaliert
+              alt="Tononónimo Logo"
+            />
+            <b fs={36}>Tononónimo</b>
+          </Group>
+        </Anchor>
         <Group gap={5} visibleFrom="xs">
           {items}
         </Group>
