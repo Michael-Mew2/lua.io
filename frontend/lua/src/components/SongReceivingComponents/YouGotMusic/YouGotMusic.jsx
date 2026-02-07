4a39cb34e82e979e1c77f1827baa1b5ec3c57aee
Michael-Mew2 <michael.mdnev@gmail.com>
2026-02-07 20:02:37 +0100

    [feature]: Add no tokens components

---
+++ frontend/lua/src/components/SongReceivingComponents/YouGotMusic/YouGotMusic.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function YouGotMusic() {
+  return (
+    <div>YouGotMusic</div>
+  )
+}
--- frontend/lua/src/pages/MusicReceive/MusicReceive.jsx
+++ frontend/lua/src/pages/MusicReceive/MusicReceive.jsx
@@ -1,11 +1,27 @@
-import * as React from 'react'
-import LoadingScreen from '../../components/LoadingScreen/LoadingScreen'
+import * as React from "react";
+import LoadingScreen from "../../components/LoadingScreen/LoadingScreen";
+import { SimpleGrid } from "@mantine/core";
+import YouGotMusic from "../../components/SongReceivingComponents/YouGotMusic/YouGotMusic";
+import ReceivedSongComment from "../../components/SongReceivingComponents/ReceivedSongComment/ReceivedSongComment";
+import NiceTry from "../../components/SongReceivingComponents/NiceTry/NiceTry";
+
 
 export default function MusicReceive() {
-    const [isLoading, setIsLoading] = React.useState(true)
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [hasEnoughTokens, setHasEnoughTokens] = React.useState(false);
+
   return (
     <>
-    {isLoading ? (<LoadingScreen />) : <div>Not loading</div> }
+      {isLoading ? (
+        <LoadingScreen />
+      ) : hasEnoughTokens ? (
+        <SimpleGrid cols={{ base: 1, md: 2 }}>
+          <YouGotMusic />
+          <ReceivedSongComment />
+        </SimpleGrid>
+      ) : (
+        <NiceTry />
+      )}
     </>
-  )
+  );
 }
