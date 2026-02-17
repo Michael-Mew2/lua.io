ffcc6cd23ff76a2cdf6f518ce630d4fcac5cbb39
Michael-Mew2 <michael.mdnev@gmail.com>
2026-02-17 16:37:54 +0100

    [feature]: force redirection for users trying to enter '/share'

--- frontend/lua/src/pages/MusicShare/MusicShare.jsx
+++ frontend/lua/src/pages/MusicShare/MusicShare.jsx
@@ -5,9 +5,21 @@ import SongShareInator from "../../components/SongSharingComponents/SongShareIna
 import SongShareInatorInstructions from "../../components/SongSharingComponents/SongShareInatorInstructions/SongShareInatorInstructions";
 import SharedSongPreview from "../../components/SongSharingComponents/SharedSongPreview/SharedSongPreview";
 import SharedSongComment from "../../components/SongSharingComponents/SharedSongComment/SharedSongComment";
+import { ReceiveSongContext } from "../../contextx/ReceiveSongContext";
+import { useAuth } from "../../contextx/useAuth";
 
 export default function MusicShare() {
   const [sharedSong, setSharedSong] = React.useState(null);
+  const {hasEnoughTokens} = React.useContext(ReceiveSongContext)
+  const {user} = useAuth();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    console.log("Need to rate a song first?", hasEnoughTokens);
+    if(hasEnoughTokens) {
+      navigate(`/members/${user.username}/claim`)
+    }
+  }, [hasEnoughTokens])
 
   return (
     <>
